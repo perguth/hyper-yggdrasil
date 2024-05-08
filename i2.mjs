e529a7b90a5c0b5428df6b1441928896d5b07ed8
Per Guth <mail@perguth.de>
2024-05-09 00:19:17 +0200

    Working i2.mjs

--- i2.mjs
+++ i2.mjs
@@ -1,17 +1,30 @@
 import net from 'net';
 import DHT from 'hyperdht'
+import EventEmitter from 'events'
 
 const node = new DHT()
+const e = new EventEmitter()
 
-const hyperSocket = node.connect(
-'c398456445c238e56ec9a6ddd0aecb33e527b33ecf16293d53106e0f8cadbeb7'
-)
+let hyperSocket = node.connect(process.argv[2])
 
+hyperSocket.on('open', function () {
+  console.log('Client connected!')
+})
 
 var netServer = net.createServer(function(netSocket) {
-	netSocket.pipe(hyperSocket).pipe(netSocket)
+        console.log('listening')
+        netSocket.on('data', function (data) {
+                console.log('net:', data)
+		hyperSocket.write(data)
+        })
+        hyperSocket.on('data', function (data) {
+                console.log('hyper:', data)
+                netSocket.write(data)
+        })
+	hyperSocket.on('close', _ => {
+		hyperSocket = node.connect(process.argv[2])
+	})
 })
 
 
-// Yggdrasil verbindet hier
 netServer.listen(3001, '127.0.0.1');
